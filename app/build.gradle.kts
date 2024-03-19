a91b9cde6fd4b5e63f3b9bb41d4e827147c09725
TonyGrif <TonyGriffin2000@gmail.com>
2024-03-19 17:16:04 -0400

    build: testing functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     application
+
+    jacoco
+    id ("org.barfuin.gradle.jacocolog") version "3.1.0"
 }
 
 repositories {
@@ -29,7 +32,22 @@ tasks.jar {
     archiveBaseName.set("vacuum-agent")
 }
 
+/* Testing Configurations */
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed", "standardOut", "standardError")
+    }
+
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
 }
