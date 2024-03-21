17e36cba59c581e7a83fd416d4d70fdaab3ad3aa
TonyGrif <TonyGriffin2000@gmail.com>
2024-03-21 17:46:44 -0400

    create environment for agent

--- app/src/main/java/agent/Environment.java
+++ app/src/main/java/agent/Environment.java
@@ -1,14 +1,25 @@
 package agent;
 
+/** Maintains the state of the Environment. */
 public class Environment {
   /** Two Tiles making up the Environment. Left Tile is the 0 element and Right is the 1 element. */
-  private Tile[] tiles;
+  private final Tile[] tiles;
 
   /** Default constructor for an Environment object. */
   public Environment() {
     tiles = new Tile[2];
     this.tiles[0] = new Tile();
     this.tiles[1] = new Tile();
+  }
+
+  /**
+   * Update the Tiles with a probability of becoming dirty.
+   *
+   * @param prob the probability the Tiles will become dirty.
+   */
+  public void updateTiles(int prob) {
+    this.getLeftTile().randSetStatus(prob);
+    this.getRightTile().randSetStatus(prob);
   }
 
   /**
--- app/src/test/java/agent/EnvTest.java
+++ app/src/test/java/agent/EnvTest.java
@@ -15,8 +15,33 @@ public class EnvTest {
 
   @Test
   public void testConstructor() {
-    assertFalse(env.getLeftTile().isDirty(), "Error in Tile constructor");
-    assertFalse(env.getRightTile().isDirty(), "Error in Tile constructor");
+    assertFalse(env.getLeftTile().isDirty(), "Error in Tile constructor.");
+    assertFalse(env.getRightTile().isDirty(), "Error in Tile constructor.");
     assertFalse(env.hasDirtyTile(), "Error initializing Tiles.");
+  }
+
+  @Test
+  public void testDirtyCheck() {
+    assertFalse(env.hasDirtyTile(), "Error initializing Tiles.");
+
+    env.getRightTile().randSetStatus(100);
+    assertTrue(env.hasDirtyTile(), "Error in update probability.");
+    env.getLeftTile().randSetStatus(100);
+    assertTrue(env.hasDirtyTile(), "Error in update probability.");
+
+    Environment leftEnv = new Environment();
+    leftEnv.getLeftTile().randSetStatus(100);
+    assertTrue(env.hasDirtyTile(), "Error in update probability.");
+  }
+
+  @Test
+  public void testUpdate() {
+    env.updateTiles(0);
+    assertFalse(env.getLeftTile().isDirty(), "Error in update probability.");
+    assertFalse(env.getRightTile().isDirty(), "Error in update probability.");
+
+    env.updateTiles(100);
+    assertTrue(env.getLeftTile().isDirty(), "Error in update probability.");
+    assertTrue(env.getRightTile().isDirty(), "Error in update probability.");
   }
 }
